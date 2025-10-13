59b9185a516caa66326f6a5ce8615637669af869
baker815 <109573420+baker815@users.noreply.github.com>
2025-10-12 21:53:34 -0400

    added authentication to student
    ugly but working

--- app/src/main/java/com/example/math_tutor_application/StudentActivity.java
+++ app/src/main/java/com/example/math_tutor_application/StudentActivity.java
@@ -1,6 +1,7 @@
 package com.example.math_tutor_application;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -44,7 +45,7 @@ public class StudentActivity extends AppCompatActivity {
 
         EditText firstNameText = findViewById(R.id.firstName);
         EditText lastNameText = findViewById(R.id.lastName);
-        EditText emailText = findViewById(R.id.emal);
+        EditText emailText = findViewById(R.id.email);
         EditText passwordText = findViewById(R.id.password);
         EditText phoneNumberText = findViewById(R.id.phoneNumber);
         EditText fieldOfStudyText = findViewById(R.id.fieldOfStudy);
@@ -59,6 +60,38 @@ public class StudentActivity extends AppCompatActivity {
 
          if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || phoneNumber.isEmpty() || programOfStudy.isEmpty()) {
              TextView errorText = findViewById(R.id.errorText);
+
+             if (firstName.isEmpty()) {
+                 firstNameText.setError("Please enter a valid first name.");
+                 firstNameText.setBackgroundColor(Color.parseColor("#FF0000"));
+
+             }
+             if (lastName.isEmpty()) {
+                 lastNameText.setError("Please enter a valid last name.");
+                 lastNameText.setBackgroundColor(Color.parseColor("#FF0000"));
+             }
+
+             if (email.isEmpty()) {
+                 emailText.setError("Please enter a valid email.");
+                 emailText.setBackgroundColor(Color.parseColor("#FF0000"));
+
+
+             }
+             if (password.isEmpty()) {
+                 passwordText.setError("Please enter a valid password.");
+                 passwordText.setBackgroundColor(Color.parseColor("#FF0000"));
+             }
+
+             if (phoneNumber.isEmpty()) {
+                 phoneNumberText.setError("Please enter a valid phone number.");
+                 phoneNumberText.setBackgroundColor(Color.parseColor("#FF0000"));
+             }
+
+            if (programOfStudy.isEmpty()) {
+                fieldOfStudyText.setError("Please enter a valid field of study.");
+                fieldOfStudyText.setBackgroundColor(Color.parseColor("#FF0000"));
+            }
+
              errorText.setText("Please fill in all fields");
              return;
          } else {
