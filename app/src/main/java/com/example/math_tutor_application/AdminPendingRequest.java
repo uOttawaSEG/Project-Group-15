27bd9fbc7bd78aaf986be286cb8d2fe1a30e37cf
baker815 <109573420+baker815@users.noreply.github.com>
2025-10-27 13:10:25 -0400

    added some functionality to admim

--- app/src/main/java/com/example/math_tutor_application/AdminPendingRequest.java
+++ app/src/main/java/com/example/math_tutor_application/AdminPendingRequest.java
@@ -8,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Dashboard_admin extends AppCompatActivity {
+public class AdminPendingRequest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard_admin);
+        setContentView(R.layout.activity_admin_pending_request);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        //
-
-
-
-
-
-
-
     }
 }
