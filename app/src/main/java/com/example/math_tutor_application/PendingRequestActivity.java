bddfe2b5715b671228b0da6b4205a006cd3d98f0
baker815 <109573420+baker815@users.noreply.github.com>
2025-11-10 16:02:18 -0500

    Update PendingRequestActivity.java

--- app/src/main/java/com/example/math_tutor_application/PendingRequestActivity.java
+++ app/src/main/java/com/example/math_tutor_application/PendingRequestActivity.java
@@ -101,11 +101,6 @@ public class PendingRequestActivity extends AppCompatActivity {
                                     adapter.notifyDataSetChanged();
 
                                 }
-                            }).addOnCompleteListener(task3 -> {
-                                //Tells the user if the list is empty so it doesn't look like a bug
-                                if (requestList.isEmpty()) {
-                                    Toast.makeText(this, "No pending requests", Toast.LENGTH_SHORT).show();
-                                }
                             });
 
                         }
@@ -113,6 +108,12 @@ public class PendingRequestActivity extends AppCompatActivity {
                         Log.e("Firestore", "Error fetching documents", task.getException());
                         Toast.makeText(this, "Failed to load requests", Toast.LENGTH_SHORT).show();
                     }
+                }).addOnCompleteListener(task3 -> {
+                    //Tells the user if the list is empty so it doesn't look like a bug
+                    if (requestList.isEmpty()) {
+                        Toast.makeText(this, "No pending requests", Toast.LENGTH_SHORT).show();
+                    }
+
                 });
     }
 
